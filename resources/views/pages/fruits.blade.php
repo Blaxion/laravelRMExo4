d532b061cce6f543b360e043fb50d3334a233ad2
Blaxion <khibaziev.f@gmail.com>
2021-07-19 14:46:09 +0200

    bg-primary

--- resources/views/pages/fruits.blade.php
+++ resources/views/pages/fruits.blade.php
@@ -1,3 +1,21 @@
 @extends('layout.index')
 @section('main')
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Quantity</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($fruits as $fruit)
+                <tr class="{{ strlen($fruit->name) >= 5 ? 'bg-primary' : '' }}">
+                    <th scope="row">{{ $fruit->id }}</th>
+                    <td>{{ $fruit->name }}</td>
+                    <td>{{ $fruit->quantity }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
